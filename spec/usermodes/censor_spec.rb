75423f98734fdd90caae577b439501dbfd03947a
Bram Matthys <syzop@vulnscan.org>
2015-10-05 14:06:09 +0200

    Add test for usermode +G (censor)

---
+++ spec/usermodes/censor_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'User Mode G censor' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+    @cbot2 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot2')
+  end
+
+  it 'should censor bad words' do
+    @swarm.perform do
+      @cbot2.send("MODE cbot2 +G")
+      sleep(2)
+      @cbot1.send("PRIVMSG cbot2 :aa fucked bb")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot2.received_pattern(/fuck/)).not_to eq(true)
+    expect(@cbot2.received_pattern(/aa <censored> bb/)).to eq(true)
+  end
+
+  it 'should not censor good words' do
+    @swarm.perform do
+      @cbot2.send("MODE cbot2 +G")
+      sleep(2)
+      @cbot1.send("PRIVMSG cbot2 :sunshine")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot2.received_pattern(/sunshine/)).to eq(true)
+  end
+
+end
