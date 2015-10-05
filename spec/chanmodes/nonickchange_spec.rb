4d0b124f26d76a0129cd7efe1818ac90e3225ef6
Bram Matthys <syzop@vulnscan.org>
2015-10-05 17:55:05 +0200

    Add test for channel mode +N (nonickchange)

---
+++ spec/chanmodes/nonickchange_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Channel Mode N (nonickchange)' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+  end
+
+  it 'should disallow NOTICE' do
+    @swarm.perform do
+      @obot.send("JOIN #nickchange")
+      @obot.send("MODE #nickchange +N")
+      sleep(2)
+      @cbot1.send("JOIN #nickchange")
+      @cbot1.send("NICK nonickchg")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/Can not change nickname/)).to eq(true)
+    expect(@obot.received_pattern(/nonickchg/)).not_to eq(true)
+  end
+end
