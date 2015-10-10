a2088a99df5c06205c764ee72598664d00d6cef5
Bram Matthys <syzop@vulnscan.org>
2015-10-10 11:23:33 +0200

    Add test for user mode +q (nokick)

---
+++ spec/usermodes/nokick_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'User Mode q (nokick)' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+  end
+
+  it 'should disallow KICK' do
+    @swarm.perform do
+      @obot.send("OPER netadmin test")
+      @obot.send("JOIN #test")
+      @obot.send("MODE obot +q")
+      sleep(1)
+      @cbot1.send("JOIN #test")
+      sleep(3)
+      @obot.send("MODE #test +o cbot1")
+      sleep(5)
+      @cbot1.send("KICK #test obot")
+      sleep(5)
+    end
+    @swarm.execute
+    expect(@obot.received_pattern(/cbot1 tried to kick you/)).to eq(true)
+    expect(@cbot1.received_pattern(/user is unkickable/)).to eq(true)
+    expect(@obot.received_pattern(/:cbot1.*KICK/)).not_to eq(true)
+  end
+end
