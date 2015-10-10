be4dd2ef97e35854519da4c75f8b3e17b97fa4d9
Bram Matthys <syzop@vulnscan.org>
2015-10-10 11:43:28 +0200

    Add test for channel mode +M (regonlymsg). Incomplete.

---
+++ spec/chanmodes/regonlymsg_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'Channel Mode M (regonlymsg)' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+  end
+
+  it 'should disallow messages from users who are not identified to services' do
+    @swarm.perform do
+      @obot.send("JOIN #test")
+      @obot.send("MODE #test +M")
+      sleep(1)
+      @cbot1.send("JOIN #test")
+      sleep(1)
+      @cbot1.send("PRIVMSG #test :this should be rejected")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/404.*You must have a registered nick/)).to eq(true)
+    expect(@obot.received_pattern(/this should be rejected/)).not_to eq(true)
+  end
+
+  it 'should allow messages from users who are identified to services'
+
+end
