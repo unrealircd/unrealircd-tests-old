3b1dff847c7704f6e0d356ee5646a75970a87cc9
Bram Matthys <syzop@vulnscan.org>
2015-10-10 11:36:25 +0200

    Add test for channel mode +O (operonly)

---
+++ spec/chanmodes/operonly_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Channel Mode O (operonly)' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+  end
+
+  it 'should disallow non-ircops' do
+    @swarm.perform do
+      @obot.send("OPER netadmin test")
+      @obot.send("JOIN #test")
+      @obot.send("MODE #test +O")
+      sleep(3)
+      @cbot1.send("JOIN #test")
+      sleep(3)
+    end
+    @swarm.execute
+    expect(@obot.received_pattern(/:cbot1.*JOIN/)).not_to eq(true)
+    expect(@cbot1.received_pattern(/520.*Cannot join channel/)).to eq(true)
+  end
+end
