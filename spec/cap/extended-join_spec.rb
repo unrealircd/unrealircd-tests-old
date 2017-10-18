d6467ab0b4fa99758f64205e55eaf5d0a850b2e7
Bram Matthys <syzop@vulnscan.org>
2017-10-18 17:04:10 +0200

    Add test for extended-join

---
+++ spec/cap/extended-join_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'CAP extended-join' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+    @cbot2 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot2', name: 'This is my realname')
+  end
+
+  it 'should show gecos in extended-join' do
+    @swarm.perform do
+      @cbot1.send("CAP REQ extended-join")
+      @cbot1.send("JOIN #test")
+      sleep(1)
+      @cbot2.send("JOIN #test")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/:cbot2.* JOIN #test \* :This is my realname/)).to eq(true)
+  end
+
+  it 'should not show gecos without extended-join' do
+    @swarm.perform do
+      @cbot1.send("JOIN #test")
+      sleep(1)
+      @cbot2.send("JOIN #test")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/:cbot2.*This is my realname/)).not_to eq(true)
+    expect(@cbot1.received_pattern(/:cbot2.* JOIN.*#test/)).to eq(true)
+  end
+end
