defa69173d5bce560984a08a946a30be4ec0f994
Bram Matthys <syzop@vulnscan.org>
2017-10-23 10:31:55 +0200

    Add test: Channel Mode i (invite only)

---
+++ spec/chanmodes/inviteonly_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'Channel Mode i (invite only)' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+    @cbot1 = @swarm.fly(server: server.host, port: server.port, nick: 'cbot1')
+  end
+
+  it 'should not allow joins with +i' do
+    @swarm.perform do
+      @obot.send("JOIN #test")
+      @obot.send("MODE #test +i")
+      sleep(1)
+      @cbot1.send("JOIN #test")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/473.*Cannot join/)).to eq(true)
+    expect(@obot.received_pattern(/:cbot1.*JOIN/)).not_to eq(true)
+  end
+
+  it 'should allow joins without -i' do
+    @swarm.perform do
+      @obot.send("JOIN #test")
+      @obot.send("MODE #test -i")
+      sleep(1)
+      @cbot1.send("JOIN #test")
+      sleep(2)
+    end
+    @swarm.execute
+    expect(@cbot1.received_pattern(/473.*Cannot join/)).not_to eq(true)
+    expect(@obot.received_pattern(/:cbot1.*JOIN/)).to eq(true)
+  end
+end
