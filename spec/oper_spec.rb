9a0be68b2680e993b8fa398f351e672cbcf0fee9
Bram Matthys <syzop@vulnscan.org>
2019-01-13 19:25:43 +0100

    Create opercommands/ and test OPER command.

--- spec/oper_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'Command Oper' do
-  it 'should reject unauthorized users'
-  it 'should reject bad passwords'
-  it 'should reject bad hosts with good passwords'
-  it 'should permit good hosts, users, and passwords'
-end
---
+++ spec/opercommands/oper_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe '/OPER command' do
+  before(:each) do
+    @swarm = Ircfly::Swarm.new
+    server = IRC_CONFIG.servers['primary']
+    @obot = @swarm.fly(server: server.host, port: server.port, nick: 'obot')
+  end
+
+  it 'should reject due to wrong case of oper name' do
+    @swarm.perform do
+      @obot.send("OPER NeTaDmIn test")
+      sleep(0.5)
+    end
+    @swarm.execute
+    # We expect the command to error (no o lines)
+    expect(@obot.received_pattern(/ 491 /)).to eq(true)
+    expect(@obot.received_pattern(/ 381 /)).not_to eq(true)
+  end
+
+  it 'should reject due to bad host' do
+    @swarm.perform do
+      @obot.send("OPER netadmin-badhost test")
+      sleep(0.5)
+    end
+    @swarm.execute
+    # We expect the command to error (no o lines)
+    expect(@obot.received_pattern(/ 491 /)).to eq(true)
+    expect(@obot.received_pattern(/ 381 /)).not_to eq(true)
+  end
+
+  it 'should reject due to wrong password' do
+    @swarm.perform do
+      @obot.send("OPER netadmin TeSt")
+      sleep(0.5)
+    end
+    @swarm.execute
+    # We expect the command to error (password incorrect)
+    expect(@obot.received_pattern(/ 464 /)).to eq(true)
+    expect(@obot.received_pattern(/ 491 /)).not_to eq(true)
+    expect(@obot.received_pattern(/ 381 /)).not_to eq(true)
+  end
+
+  it 'should accept with correct name, pass and hostname' do
+    @swarm.perform do
+      @obot.send("OPER netadmin test")
+      sleep(0.5)
+    end
+    @swarm.execute
+    # We expect the command to succeed (you are now an IRC operator)
+    expect(@obot.received_pattern(/ 381 /)).to eq(true)
+    expect(@obot.received_pattern(/ 491 /)).not_to eq(true)
+  end
+
+end
