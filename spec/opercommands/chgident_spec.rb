d1e28500aab25d25f20dc33d12bfd8f6bc675c86
Bram Matthys <syzop@vulnscan.org>
2019-01-14 11:15:03 +0100

    Add CHGIDENT test, update CHGHOST test to check whois for should-fail-cases.

--- spec/opercommands/chgident_spec.rb
+++ spec/opercommands/chgident_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'CHGHOST command' do
+describe 'CHGIDENT command' do
   before(:each) do
     @swarm = Ircfly::Swarm.new
     servera = IRC_CONFIG.servers['primary']
@@ -9,37 +9,36 @@ describe 'CHGHOST command' do
     @cbot2 = @swarm.fly(server: serverb.host, port: serverb.port, nick: 'cbot2')
   end
 
-  it 'should disallow CHGHOST to normal users' do
+  it 'should disallow CHGIDENT to normal users' do
     @swarm.perform do
-      @obot.send("CHGHOST obot some.nice.new.vhost")
+      @obot.send("CHGIDENT obot changedid")
       @obot.send("WHOIS obot")
+      sleep(0.5)
     end
     @swarm.execute
     # We expect the command to error
     expect(@obot.received_pattern(/ 481 /)).to eq(true)
-    expect(@obot.received_pattern(/ some.nice.new.vhost :is now your displayed host/)).not_to eq(true)
-    expect(@obot.received_pattern(/ 311.* some.nice.new.vhost/)).not_to eq(true)
+    expect(@obot.received_pattern(/ 311.* changedid/)).not_to eq(true)
   end
 
-  it 'should disallow CHGHOST to opers with insufficient privileges' do
+  it 'should disallow CHGIDENT to opers with insufficient privileges' do
     @swarm.perform do
       @obot.send("OPER locop test")
-      @obot.send("CHGHOST obot some.nice.new.vhost")
+      @obot.send("CHGIDENT obot changedid")
       @obot.send("WHOIS obot")
     end
     @swarm.execute
     # We expect the command to error
     expect(@obot.received_pattern(/ 481 /)).to eq(true)
-    expect(@obot.received_pattern(/ some.nice.new.vhost :is now your displayed host/)).not_to eq(true)
-    expect(@obot.received_pattern(/ 311.* some.nice.new.vhost/)).not_to eq(true)
+    expect(@obot.received_pattern(/ 311.* changedid/)).not_to eq(true)
   end
 
   # We run this one one a remote user, to get a better test
-  it 'should allow CHGHOST to opers' do
+  it 'should allow CHGIDENT to opers' do
     @swarm.perform do
       @obot.send("OPER netadmin test")
       sleep(0.5)
-      @obot.send("CHGHOST cbot2 some.nice.new.vhost")
+      @obot.send("CHGIDENT cbot2 changedid")
       sleep(0.5)
       @cbot2.send("WHOIS cbot2")
       @obot.send("WHOIS cbot2")
@@ -47,11 +46,9 @@ describe 'CHGHOST command' do
     @swarm.execute
     # Error is not expected
     expect(@obot.received_pattern(/ 481 /)).not_to eq(true)
-    # User should get a message about the new host
-    expect(@cbot2.received_pattern(/ some.nice.new.vhost :is now your displayed host/)).to eq(true)
     # WHOIS from both servers should show the user with the new vhost
-    expect(@obot.received_pattern(/ 311.* some.nice.new.vhost/)).to eq(true)
-    expect(@cbot2.received_pattern(/ 311.* some.nice.new.vhost/)).to eq(true)
+    expect(@obot.received_pattern(/ 311.* changedid/)).to eq(true)
+    expect(@cbot2.received_pattern(/ 311.* changedid/)).to eq(true)
   end
 
 end
